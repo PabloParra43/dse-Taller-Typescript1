20c32521d55be3f558ad8d5bfb48f60e3b3986af
PabloParra43 <p.parrac@uniandes.edu.co>
2025-04-06 15:09:36 -0500

    Taller 1

---
+++ dist/Serie.js
@@ -0,0 +1,11 @@
+export class Serie {
+    constructor(id, name, channel, seasons, synopsis, url, image) {
+        this.id = id;
+        this.name = name;
+        this.channel = channel;
+        this.seasons = seasons;
+        this.synopsis = synopsis;
+        this.url = url;
+        this.image = image;
+    }
+}
